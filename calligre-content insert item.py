a76817232d3cbf205e43e91e3cf5007ecd6cccd5
Kyle Lexmond <github@kyl191.net>
2016-09-22 23:12:09 -0700

    Fix up add item

--- calligre-content insert item.py
+++ calligre-content insert item.py
@@ -1,30 +1,39 @@
 import boto3, time, pprint
+import json
 from decimal import Decimal
 from boto3.dynamodb.conditions import Attr
 
+dynamo = boto3.resource('dynamodb', region_name="us-west-2").Table("calligre-posts")
+
 def lambda_handler(event, context):
     print("Received event: " + pprint.pformat(event))
-    dynamo = boto3.resource('dynamodb', region_name="us-west-2").Table("calligre-posts")
     timestamp = Decimal(time.time())
-    details = event.get("body-json", {})
+    body = event.get("body")
+    try:
+        details = json.loads(body)
+    except:
+        return {"statusCode": 400, "body": "Provide a body please"}
     item = {
         "posts": "posts",
         "timestamp" : timestamp,
-        "poster_id" : details.get("poster_id"),
+        "poster_id" : "temp id",
         "like_count" : 0,
         "text": details.get("text", "No text provided")
     }
+
     media_link = details.get("media_link")
     if media_link:
         item['media_link'] = media_link
-    print(r)
+
     r = dynamo.put_item(
         Item=item,
         ConditionExpression=Attr("timestamp").ne(timestamp)
     )
+    response = {"id": str(timestamp)}
+
     return {
-        "id": str(timestamp),
-        "statusCode": r.get("ResponseMetadata", {}).get("HTTPStatusCode", 500)
+        "statusCode": r.get("ResponseMetadata", {}).get("HTTPStatusCode", 500),
+        "body":json.dumps(response)
     }
 
-# curl -H "Content-Type: application/json" -v -X POST -d '{"poster":12348,"text":"asjdhdjflasjdfas","media_link":"not-a-link-also"}' https://calligre.com/api/content
+# curl -H "Content-Type: application/json" -v -X POST -d '{"text":"asjdhdjflasjdfas","media_link":"not-a-link-also"}' https://calligre.com/api/content
