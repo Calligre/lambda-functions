f85d95719a2b44ec98f624eb83b8c50b7cb58d3c
Kyle Lexmond <github@kyl191.net>
2016-09-23 00:10:36 -0700

    Remove duplicate file

--- create-content-dynamo-table.py
+++ create-content-dynamo-table.py
@@ -1,17 +1,17 @@
 import boto3
 
-ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
-ddb.create_table(
+db = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+db.create_table(
     TableName="calligre-posts",
     KeySchema=[
         # This is horrible DynamoDB, but we're only going to be using 1, maybe 2 partitions max
         # With 1|2 partitions, hot partitions aren't much of an issue
-        { "AttributeName": "timestamp", "KeyType": "HASH" },
-        { "AttributeName": "uuid", "KeyType": "RANGE" }
+        { "AttributeName": "posts", "KeyType": "HASH" },
+        { "AttributeName": "timestamp", "KeyType": "RANGE" }
     ],
     AttributeDefinitions=[
         { "AttributeName": "timestamp", "AttributeType": "N" },
-        { "AttributeName": "uuid", "AttributeType": "S" },
+        { "AttributeName": "posts", "AttributeType": "S" },
 
     ],
     ProvisionedThroughput={
@@ -19,3 +19,5 @@ ddb.create_table(
         "WriteCapacityUnits": 5
     }
 )
+
+# db.delete_table(TableName="calligre-posts")
