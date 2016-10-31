c6702627b6f352e860ee2da04e0b1d0657291893
Kyle Lexmond <github@kyl191.net>
2016-10-30 20:58:43 -0700

    make pytest happy

--- PostToExternalServices.py
+++ PostToExternalServices.py
@@ -10,79 +10,74 @@ AUTH0_BASE = "https://calligre.auth0.com"
 FB_BASE = "https://graph.facebook.com/v2.7"
 S3_BUCKET = "calligre-media"
 
-AUTH0_CLIENT_ID = None
-AUTH0_CLIENT_SECRET = None
-AUTH0_TOKEN = None
+s3_client = boto3.client('s3')  # pylint: disable=C0103
 
-TWITTER_CLIENT_ID = None
-TWITTER_CLIENT_SECRET = None
+secrets = {}  # pylint: disable=C0103
 
-log = logging.getLogger()
+log = logging.getLogger()  # pylint: disable=C0103
 log.setLevel(logging.DEBUG)
-s3_client = boto3.client('s3')
 
 
-def setAuth0ClientDetails():
-    global AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET
+def set_auth0_client_details():
     log.info("Fetching Auth0 client details")
     try:
         response = s3_client.get_object(
             Bucket="calligre-config",
             Key="auth0_token.json",
         )
-    except Exception as e:
-        log.error(e)
-        raise e
+    except Exception as ex:
+        log.error(ex)
+        raise ex
 
     content = response.get("Body").read()
     tokens = json.loads(content)
     if not tokens.get("AUTH0_CLIENT_ID"):
         log.error("Didn't fetch Auth0 client settings!")
-    AUTH0_CLIENT_ID = tokens["AUTH0_CLIENT_ID"]
-    AUTH0_CLIENT_SECRET = tokens["AUTH0_CLIENT_SECRET"]
+    secrets["AUTH0_CLIENT_ID"] = tokens["AUTH0_CLIENT_ID"]
+    secrets["AUTH0_CLIENT_SECRET"] = tokens["AUTH0_CLIENT_SECRET"]
 
 
-def setAuth0Token():
-    global AUTH0_TOKEN
+def set_auth0_token():
     log.info("Fetching Auth0 token")
-    if not AUTH0_CLIENT_ID or not AUTH0_CLIENT_SECRET:
-        setAuth0ClientDetails()
+    if not (secrets.get("AUTH0_CLIENT_ID") and
+            secrets.get("AUTH0_CLIENT_SECRET")):
+        set_auth0_client_details()
     payload = {
-        "client_id": AUTH0_CLIENT_ID,
-        "client_secret": AUTH0_CLIENT_SECRET,
+        "client_id": secrets.get("AUTH0_CLIENT_ID"),
+        "client_secret": secrets.get("AUTH0_CLIENT_SECRET"),
         "audience": "%s/api/v2/" % AUTH0_BASE,
         "grant_type": "client_credentials"
     }
     try:
         token = requests.post("%s/oauth/token" % AUTH0_BASE, json=payload).\
             json()
-    except Exception as e:
-        log.error(e)
-        raise e
-    AUTH0_TOKEN = token.get("access_token")
-    if not AUTH0_TOKEN:
+    except Exception as ex:
+        log.error(ex)
+        raise ex
+    secrets["AUTH0_TOKEN"] = token.get("access_token")
+    if not secrets["AUTH0_TOKEN"]:
         log.error("Failed to fetch Auth0 Token!")
         log.error(token)
         raise Exception
-    log.info("Auth0 token set: %s", AUTH0_TOKEN)
+    log.info("Auth0 token set: %s", secrets["AUTH0_TOKEN"])
 
 
-def getAuth0UserTokens(user_id):
-    if not AUTH0_TOKEN:
-        setAuth0Token()
-    headers = {"Authorization": "Bearer %s" % AUTH0_TOKEN}
+def get_auth0_user_tokens(user_id):
+    if not secrets.get("AUTH0_TOKEN"):
+        set_auth0_token()
+    headers = {"Authorization": "Bearer %s" % secrets.get("AUTH0_TOKEN")}
     try:
         return requests.get("%s/api/v2/users/%s" % (AUTH0_BASE, user_id),
                             headers=headers).\
                             json().\
                             get("identities")
 
-    except Exception as e:
-        log.error(e)
-        raise e
+    except Exception as ex:
+        log.error(ex)
+        raise ex
 
 
-def postFbMessage(user_token, message):
+def post_fb_message(user_token, message):
     fb_data = {
         "message": message,
         "access_token": user_token
@@ -90,7 +85,7 @@ def postFbMessage(user_token, message):
     requests.post("%s/me/feed" % FB_BASE, data=fb_data)
 
 
-def postFbPhoto(user_token, message, file_path):
+def post_fb_photo(user_token, message, file_path):
     fb_photo_data = {
         "caption": message,
         "access_token": user_token
@@ -99,32 +94,33 @@ def postFbPhoto(user_token, message, file_path):
     requests.post("%s/me/photos" % FB_BASE, data=fb_photo_data, files=files)
 
 
-def setTwitterClientDetails():
-    global TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET
+def set_twitter_client_details():
     log.info("Fetching Twitter client details")
     try:
         response = s3_client.get_object(
             Bucket="calligre-config",
             Key="twitter_token.json",
         )
-    except Exception as e:
-        log.error(e)
-        raise e
+    except Exception as ex:
+        log.error(ex)
+        raise ex
 
     content = response.get("Body").read()
     tokens = json.loads(content)
     if not tokens.get("TWITTER_CLIENT_ID"):
         log.error("Didn't fetch Twitter client settings!")
         raise Exception
-    TWITTER_CLIENT_ID = tokens["TWITTER_CLIENT_ID"]
-    TWITTER_CLIENT_SECRET = tokens["TWITTER_CLIENT_SECRET"]
+    secrets["TWITTER_CLIENT_ID"] = tokens["TWITTER_CLIENT_ID"]
+    secrets["TWITTER_CLIENT_SECRET"] = tokens["TWITTER_CLIENT_SECRET"]
 
 
-def postTwMessage(access_token, access_secret, message, media_path=None):
+def post_tw_message(access_token, access_secret, message, media_path=None):
     # dev ref: https://dev.twitter.com/rest/reference/post/statuses/update
-    if not TWITTER_CLIENT_ID or not TWITTER_CLIENT_SECRET:
-        setTwitterClientDetails()
-    auth = tweepy.OAuthHandler(TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET)
+    if not (secrets.get("TWITTER_CLIENT_ID") and
+            secrets.get("TWITTER_CLIENT_SECRET")):
+        set_twitter_client_details()
+    auth = tweepy.OAuthHandler(secrets.get("TWITTER_CLIENT_ID"),
+                               secrets.get("TWITTER_CLIENT_SECRET"))
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
 
@@ -135,21 +131,10 @@ def postTwMessage(access_token, access_secret, message, media_path=None):
     log.debug(status)
 
 
-def postTwPhoto(access_token, access_secret, media_link):
-    # get photo from S3
-    # post to upload.twitter.com
-    # init - https://dev.twitter.com/rest/reference/post/media/upload-init
-    # append - https://dev.twitter.com/rest/reference/post/media/upload-append
-    # finalize -
-    # https://dev.twitter.com/rest/reference/post/media/upload-finalize
-    # return media_id
-    return None
-
-
 def handler(event, _):
-    for m in event["Records"]:
-        log.debug(m)
-        sns = m.get("Sns")
+    for record in event["Records"]:
+        log.debug(record)
+        sns = record.get("Sns")
         if not sns:
             continue
         text = sns.get("Message")
@@ -171,14 +156,14 @@ def handler(event, _):
             with open(file_path, "wb") as data:
                 s3_client.download_fileobj(S3_BUCKET, media_s3, data)
 
-        for network in getAuth0UserTokens(userid):
+        for network in get_auth0_user_tokens(userid):
             if network.get("connection") == "facebook" and post_fb:
                 access_token = network.get("access_token")
                 if media_s3:
-                    postFbPhoto(access_token, text, file_path)
+                    post_fb_photo(access_token, text, file_path)
                 else:
-                    postFbMessage(access_token, text)
+                    post_fb_message(access_token, text)
             elif network.get("connection") == "twitter" and post_tw:
                 access_token = network.get("access_token")
                 access_secret = network.get("access_token_secret")
-                postTwMessage(access_token, access_secret, text, file_path)
+                post_tw_message(access_token, access_secret, text, file_path)
--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if [ ! -d "requests-2.11.1.dist-info" ]; then
 fi
 rm -f ext-services.zip
 # Would be nice to exclude all the .py files in favour of the .pyc files
-zip -r ext-services.zip oauthlib requests requests_oauthlib tweepy six.pyc post-to-external-services.py
+zip -r ext-services.zip oauthlib requests requests_oauthlib tweepy six.pyc PostToExternalServices.py
 aws s3 cp ext-services.zip "s3://calligre/ext-services.zip"
 aws lambda update-function-code --function-name calligre-external-posts --s3-bucket calligre --s3-key "ext-services.zip" --publish --region us-west-2
