a0d45d7151fbc907cfad627a76c6220f488deb4f
Kyle Lexmond <github@kyl191.net>
2016-10-30 21:23:55 -0700

    strip down the deployment package

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "requests-2.11.1.dist-info" ]; then
-    pip install -t . -r requirements.txt
+export DEP_FOLDER="ext-posts-dependencies"
+if [ ! -d "${DEP_FOLDER}" ]; then
+    mkdir -p ${DEP_FOLDER}
+fi
+if [ ! -d "${DEP_FOLDER}/requests-2.11.1.dist-info" ]; then
+    pip install -t ${DEP_FOLDER} -r requirements.txt
+    find ${DEP_FOLDER} -name '*.py' -delete
 fi
 rm -f ext-services.zip
-# Would be nice to exclude all the .py files in favour of the .pyc files
-zip -r ext-services.zip oauthlib requests requests_oauthlib tweepy six.pyc PostToExternalServices.py
+cd ${DEP_FOLDER}
+zip -r ../ext-services.zip oauthlib requests requests_oauthlib tweepy six.pyc
+cd ..
+zip -r ext-services.zip PostToExternalServices.py
 aws s3 cp ext-services.zip "s3://calligre/ext-services.zip"
 aws lambda update-function-code --function-name calligre-external-posts --s3-bucket calligre --s3-key "ext-services.zip" --publish --region us-west-2
