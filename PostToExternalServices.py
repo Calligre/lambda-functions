f43f58c3330d27cbb6375f7904cb91ae982efdee
Kyle Lexmond <github@kyl191.net>
2016-10-31 22:45:14 -0700

    Think I addressed everything...

--- PostToExternalServices.py
+++ PostToExternalServices.py
@@ -7,8 +7,10 @@ import requests
 import tweepy
 
 AUTH0_BASE = "https://calligre.auth0.com"
+AUTH0_API = "%s/api/v2/" % AUTH0_BASE
 FB_BASE = "https://graph.facebook.com/v2.7"
 S3_BUCKET = "calligre-media"
+S3_CONFIG_BUCKET = "calligre-config"
 
 s3_client = boto3.client('s3')  # pylint: disable=C0103
 
@@ -22,46 +24,46 @@ def set_secrets():
     log.info("Fetching secrets from S3")
     try:
         response = s3_client.get_object(
-            Bucket="calligre-config",
+            Bucket=S3_CONFIG_BUCKET,
             Key="secrets.json",
         )
     except Exception as ex:
-        log.error(ex)
+        log.exception(ex)
         raise ex
 
-    content = response.get("Body").read()
+    try:
+        content = response.get("Body").read()
+    except AttributeError as ex:
+        log.error("S3 response didn't contain Body: %s", response)
+        log.exception(ex)
     retrieved_secrets = json.loads(content)
-    if len(retrieved_secrets) == 0:
-        log.error("Didn't fetch any secrets!")
-        raise Exception
-    elif len(retrieved_secrets) < 4:
+    assert len(retrieved_secrets) > 0, "Didn't fetch any secrets!"
+
+    if len(retrieved_secrets) < 4:
         log.warning("Number of secrets seems oddly small...")
     secrets.update(retrieved_secrets)
 
 
 def set_auth0_token():
     log.info("Fetching Auth0 token")
-    if not (secrets.get("AUTH0_CLIENT_ID") and
-            secrets.get("AUTH0_CLIENT_SECRET")):
-        log.error("Auth0 Client Tokens not set!")
-        raise Exception
+    assert (secrets.get("AUTH0_CLIENT_ID") and
+            secrets.get("AUTH0_CLIENT_SECRET")),\
+        "Auth0 Client Tokens not set!"
+
     payload = {
         "client_id": secrets.get("AUTH0_CLIENT_ID"),
         "client_secret": secrets.get("AUTH0_CLIENT_SECRET"),
-        "audience": "%s/api/v2/" % AUTH0_BASE,
+        "audience": AUTH0_API,
         "grant_type": "client_credentials"
     }
     try:
         token = requests.post("%s/oauth/token" % AUTH0_BASE, json=payload).\
             json()
     except Exception as ex:
-        log.error(ex)
+        log.exception(ex)
         raise ex
     secrets["AUTH0_TOKEN"] = token.get("access_token")
-    if not secrets["AUTH0_TOKEN"]:
-        log.error("Failed to fetch Auth0 Token!")
-        log.error(token)
-        raise Exception
+    assert secrets["AUTH0_TOKEN"], "Failed to fetch Auth0 Token!"
     log.info("Auth0 token set: %s", secrets["AUTH0_TOKEN"])
 
 
@@ -70,13 +72,13 @@ def get_auth0_user_tokens(user_id):
         set_auth0_token()
     headers = {"Authorization": "Bearer %s" % secrets.get("AUTH0_TOKEN")}
     try:
-        return requests.get("%s/api/v2/users/%s" % (AUTH0_BASE, user_id),
+        return requests.get("%s/users/%s" % (AUTH0_API, user_id),
                             headers=headers).\
                             json().\
                             get("identities")
 
     except Exception as ex:
-        log.error(ex)
+        log.exception(ex)
         raise ex
 
 
@@ -93,16 +95,16 @@ def post_fb_photo(user_token, message, file_path):
         "caption": message,
         "access_token": user_token
     }
-    files = {"file": open(file_path, "rb")}
-    requests.post("%s/me/photos" % FB_BASE, data=fb_photo_data, files=files)
+    with open(file_path, "rb") as media_file:
+        requests.post("%s/me/photos" % FB_BASE,
+                      data=fb_photo_data,
+                      files={"file": media_file})
 
 
 def post_tw_message(access_token, access_secret, message, media_path=None):
-    # dev ref: https://dev.twitter.com/rest/reference/post/statuses/update
-    if not (secrets.get("TWITTER_CLIENT_ID") and
-            secrets.get("TWITTER_CLIENT_SECRET")):
-        log.error("Twitter Client Tokens not set, failing!")
-        raise Exception
+    assert (secrets.get("TWITTER_CLIENT_ID") and
+            secrets.get("TWITTER_CLIENT_SECRET")),\
+            "Twitter Client Tokens not set, failing!"
     auth = tweepy.OAuthHandler(secrets.get("TWITTER_CLIENT_ID"),
                                secrets.get("TWITTER_CLIENT_SECRET"))
     auth.set_access_token(access_token, access_secret)
@@ -118,7 +120,7 @@ def post_tw_message(access_token, access_secret, message, media_path=None):
 def handler(event, _):
     if len(secrets) == 0:
         set_secrets()
-    for record in event["Records"]:
+    for record in event.get("Records"):
         log.debug(record)
         sns = record.get("Sns")
         if not sns:
@@ -126,8 +128,8 @@ def handler(event, _):
         text = sns.get("Message")
         attrs = sns.get("MessageAttributes", {})
         userid = attrs.get("userid", {}).get("Value")
-        post_fb = attrs.get("facebook", {}).get("Value", False)
-        post_tw = attrs.get("twitter", {}).get("Value", False)
+        post_fb = attrs.get("facebook", {}).get("Value")
+        post_tw = attrs.get("twitter", {}).get("Value")
 
         # parse the fb & Twitter flags, because SNS is only Strings/Ints/Bytes
         if post_fb:
@@ -139,8 +141,7 @@ def handler(event, _):
         file_path = None
         if media_s3:
             file_path = os.path.join("/tmp", media_s3)
-            with open(file_path, "wb") as data:
-                s3_client.download_fileobj(S3_BUCKET, media_s3, data)
+            s3_client.download_fileobj(S3_BUCKET, media_s3, file_path)
 
         for network in get_auth0_user_tokens(userid):
             if network.get("connection") == "facebook" and post_fb:
