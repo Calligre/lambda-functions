999ca3db8d108d64eab8386765fb2a4c2d7457f5
Kyle Lexmond <github@kyl191.net>
2016-12-04 17:29:37 -0800

    Cleanup code

--- ResizeImage.py
+++ ResizeImage.py
@@ -14,13 +14,12 @@ log.setLevel(logging.DEBUG)
 
 def handler(event, _):
     for record in event['Records']:
-        _, original = mkstemp()
         bucket = record['s3']['bucket']['name']
         # S3 keys are urlencoded in event notifications
         # https://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
         key = unquote_plus(record['s3']['object']['key'])
         log.debug("Got new file: %s:%s", bucket, key)
-        get_file(original, bucket, key)
+        original = get_file(original, bucket, key)
         resized = resize_image(original)
         put_file(resized,
                  os.environ.get('DEST_BUCKET', 'calligre-images'),
@@ -28,20 +27,24 @@ def handler(event, _):
         delete_file(bucket, key)
 
 
-def get_file(dest, bucket, key):
+def get_file(bucket, key):
+    log.debug("Fetching file: %s:%s", bucket, key)
     try:
-        log.debug("Fetching file: %s:%s", bucket, key)
+        _, dest = mkstemp()
         s3.download_file(bucket, key, dest)
+        return dest
     except Exception as ex:
         log.exception(ex)
         raise ex
 
 
 def resize_image(src):
+    # thumbnail is badly named, it'll resize the image while keping the aspect ratio
+    # We use 1024 px on the longest edge as a good balance
     size = (1024, 1024)
     _, outfile = mkstemp()
+    log.debug("Resizing: %s", src)
     try:
-        log.debug("Resizing: %s", src)
         image = Image.open(src)
         image.thumbnail(size)
         image.save(outfile, "JPEG")
@@ -61,8 +64,8 @@ def put_file(src, bucket, key):
 
 
 def delete_file(bucket, key):
+    log.debug("Deleting: %s:%s", bucket, key)
     try:
-        log.debug("Deleting: %s:%s", bucket, key)
         s3.delete_object(Bucket=bucket, Key=key)
     except Exception as ex:
         log.exception(ex)
