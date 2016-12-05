132998ef23bf763df2202b730bfb593c86664b95
Kyle Lexmond <github@kyl191.net>
2016-12-04 17:10:37 -0800

    Add logging and fix url encoding issues

--- ResizeImage.py
+++ ResizeImage.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from tempfile import mkstemp
+from urllib import unquote_plus
 
 from PIL import Image
 import boto3
@@ -14,18 +15,22 @@ log.setLevel(logging.DEBUG)
 def handler(event, _):
     for record in event['Records']:
         _, original = mkstemp()
-        name = record['s3']['object']['key']
         bucket = record['s3']['bucket']['name']
-        get_file(original, bucket, name)
+        # S3 keys are urlencoded in event notifications
+        # https://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
+        key = unquote_plus(record['s3']['object']['key'])
+        log.debug("Got new file: %s:%s", bucket, key)
+        get_file(original, bucket, key)
         resized = resize_image(original)
         put_file(resized,
                  os.environ.get('DEST_BUCKET', 'calligre-images'),
-                 name)
-        delete_file(bucket, name)
+                 key)
+        delete_file(bucket, key)
 
 
 def get_file(dest, bucket, key):
     try:
+        log.debug("Fetching file: %s:%s", bucket, key)
         s3.download_file(bucket, key, dest)
     except Exception as ex:
         log.exception(ex)
@@ -36,6 +41,7 @@ def resize_image(src):
     size = (1024, 1024)
     _, outfile = mkstemp()
     try:
+        log.debug("Resizing: %s", src)
         image = Image.open(src)
         image.thumbnail(size)
         image.save(outfile, "JPEG")
@@ -46,7 +52,7 @@ def resize_image(src):
 
 
 def put_file(src, bucket, key):
-    log.info("Putting %s into %s:%s", src, bucket, key)
+    log.debug("Putting %s into %s:%s", src, bucket, key)
     try:
         s3.upload_file(src, bucket, key)
     except Exception as ex:
@@ -56,6 +62,7 @@ def put_file(src, bucket, key):
 
 def delete_file(bucket, key):
     try:
+        log.debug("Deleting: %s:%s", bucket, key)
         s3.delete_object(Bucket=bucket, Key=key)
     except Exception as ex:
         log.exception(ex)
